3b9d2cdebc870f398da9168b738f01e0453e4440
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2025-11-25 01:51:46 +0000

    so no the layout doesn't make sense but its successfully pulling from deck data

--- living_flashcards/flashcards/views.py
+++ living_flashcards/flashcards/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.conf import settings
 
 # Create your views here.
 
 def flashcards(request, slug):
   template = loader.get_template('flashcards.html')
-  # replace with code to actually populate flashcards from .csv or API
-  terms_and_definitions = {"category1": {"cat": "idk", "dog": "idk"}, "category2": {"no": "yes"}}
-  return render(request, 'flashcards.html', {'flashcard_data': terms_and_definitions})
+  # take slug and use to get deck to pass
+  decks = settings.DECKS
+  deck_info = {}
+  for deck in decks: 
+    if deck["name"] == slug: 
+      deck_info = deck
+  # add some sort of error saying deck couldn't be found
+  return render(request, 'flashcards.html', {'deck_info': deck_info})

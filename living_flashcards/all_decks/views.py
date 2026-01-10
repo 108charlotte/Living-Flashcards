d2ed3e67e6e9575d1840532f0f8a7dccbe17b043
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-01-10 10:26:07 -0500

    fixed jinja templating

--- living_flashcards/all_decks/views.py
+++ living_flashcards/all_decks/views.py
@@ -15,5 +15,5 @@ def all_decks(request):
         now = timezone.now()
         user_cards = CardToUser.objects.filter(card_id__deck=deck, user_id=request.user)
         to_review = (user_cards.filter(see_next__lte=now) | user_cards.filter(see_next__isnull=True))
-        deck.cards_to_review = to_review.count()  # Set count for template
+        deck.cards_to_review = to_review.count()  # set count for template
     return render(request, 'all_decks.html', {'decks': decks, 'available_languages': ["English"], 'available_languages_to_learn': ["Sora"]})
--- living_flashcards/flashcards/admin.py
+++ living_flashcards/flashcards/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Deck, CardInfo
+from .models import Deck, CardInfo, CardToUser
 
 # Register your models here.
 admin.site.register(Deck)
 admin.site.register(CardInfo)
+admin.site.register(CardToUser)

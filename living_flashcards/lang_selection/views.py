83dd07811a8582db8942c736f0a892bd56279465
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-02-27 17:50:42 -0500

    fixed new user lang selection redirect issue
    - explicitly redirected to all decks
    - only allowed post requests to set language
    
    @SofiTeresa08, do you know if this issue is also present in demo-2?

--- living_flashcards/lang_selection/views.py
+++ living_flashcards/lang_selection/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST
 from .models import UserLanguage
 
 # Create your views here.
 def langselection(request): 
     return render(request, 'lang_selection.html', {'available_languages': ['English']})
 
+@require_POST
 def setlanguage(request): 
-    if request.method == "POST": 
-        if request.user.is_authenticated: 
-            language = request.POST.get('language', 'en')
-            UserLanguage.objects.update_or_create(djangousermodel=request.user, defaults={'language': language})
+    if request.user.is_authenticated: 
+        language = request.POST.get('language', 'en')
+        UserLanguage.objects.update_or_create(djangousermodel=request.user, defaults={'language': language})
 
-        return redirect('all_decks')
+    return redirect('all_decks:all_decks')
